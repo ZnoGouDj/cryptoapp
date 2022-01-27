2fe5783c3b13b3d026ee1b7a42eaf153e5f42c89
ZnoGouDj <znogoud3@gmail.com>
2022-01-27 14:41:37 +0300

    Add store, provider, services

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,20 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query';
+
+const cryptoApiHeaders = {
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+  'x-rapidapi-key': '759a24c1c6mshc1ecfe00fa9f9c3p107251jsnee515c7609b8',
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com/coins';
+
+const createRequest = url => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: builder => ({
+    getCryptos: builder.query({
+      query: () => createRequest('/exchanges'),
+    }),
+  }),
+});
